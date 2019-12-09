c917743cbec176ee8be1055b8ccc7afeee6b6d68
Virginia Wilcox <wilcoxva@gmail.com>
2019-12-08 19:23:37 -0500

    added first two listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// mouse events 
+const busImg = document.querySelector('.intro img');
+busImg.addEventListener('mouseenter', (event) => {
+  busImg.style.transform = "scale(.9)";
+  busImg.style.transition = "all 0.3s";
+});
+busImg.addEventListener('mouseleave', (event) => {
+  busImg.style.transform = "scale(1.0)";
+  busImg.style.transition = "all 0.3s";
+});
