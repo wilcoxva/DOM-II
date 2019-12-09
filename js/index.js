79967c23e6be91717086388047ed87a49d21ea93
Virginia Wilcox <wilcoxva@gmail.com>
2019-12-08 19:27:25 -0500

    added third listener

--- js/index.js
+++ js/index.js
@@ -9,3 +9,8 @@ busImg.addEventListener('mouseleave', (event) => {
   busImg.style.transform = "scale(1.0)";
   busImg.style.transition = "all 0.3s";
 });
+// resize event
+const mapPic = document.querySelector('.img-content img');
+window.addEventListener('resize', () => {
+  mapPic.src = 'https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80';
+});
